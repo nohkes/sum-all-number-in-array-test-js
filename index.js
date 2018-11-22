395fe210288c10e85dfba1ada6121ff95d832ae9
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-11-22 03:35:41 +0000

    S

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  });
+  return sum;
 }
+
+// return Array.isArray(array) ? array.length === 0 ? 0 : array[0] + sumItems(array.slice(1))
 
 module.exports = sumItems;
